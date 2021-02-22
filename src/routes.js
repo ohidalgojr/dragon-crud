56a5cabadb2d0fea4d9a098c6686f1d34ad16431
Orlando <orlandorodriguesti@gmail.com>
2021-02-22 09:16:48 -0300

    Initial authentication

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 
-import { CreateDragon, Dragons } from "./pages";
+import { CreateDragon, Dragons, Login } from "./pages";
+import { isAuthenticated } from "./services/auth";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: "/login", state: { from: props.location } }}
+        />
+      )
+    }
+  />
+);
 
 const Routes = () => (
   <BrowserRouter>
-    <Route exact path="/" component={Dragons} />
-    <Route exact path="/create" component={CreateDragon} />
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <PrivateRoute exact path="/" component={Dragons} />
+      <PrivateRoute exact path="/create" component={CreateDragon} />
+      <Route render={() => <Redirect to="/" />} />
+    </Switch>
   </BrowserRouter>
 );
 
